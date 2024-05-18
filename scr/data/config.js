ab7a11d285f6c9618767382764aa91c7070da08e
Burak <burakofficial065@gmail.com>
2024-05-18 22:14:57 +0300

    Add files via upload

---
+++ scr/data/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    
+
+    ACTIVITY_NAME: "discord.gg/aDYrCFPvmM",
+    BOT_SAHIPLERI: ["1152342725170057237"],
+    SELFBOT_TOKENS: ["OWO GÖNDERİM HESAP","OWO GÖNDERİM HESAP TOKEN 2"],
+    
+    DISCORD_BOT_TOKEN: "BOT TOKEN",
+    DISCORD_CLIENT_ID: "1220750396205695016",
+    MONGO_URI: "", // Youtubeden araştırarak alabilirsniz
+    
+    SUNUCU_ID: "1221917649966076076",
+    TESLIMAT_KANALI_ID: "1221919495325483068",
+    LOG_KANALI_ID: "1221919538140942356",
+    LOG_CHANNEL_WEBHOOK_LINK: "webhok",
+    SISTEM_LOG_KANALI_ID: "1221919596261412959",
+    KOD_YAZILACAK_TESLIM_KANALI: "1221919663131201766",
+
+    TESLIMAT_SEKLI: "owo give {user} {cash}",
+    WEBHOOK_KOD_ONAYLANDI_BILDIRIMI: "# {emoji} Kod onaylandı {member}, lütfen bekleyin.",
+    WEBHOOK_TESLIM_BILDIRIMI: ":bell: __İşlem tamamlandı,__ **owo cash** __yazarak gelip gelmediğini kontrol edin.__\n\n<a:shop_kalp:1188603078992199700> `Siparişinizi onaylayıp değerlendirme yapmayı unutmayın!`",
+    WEBHOOK_HATA_BILDIRIMI: "Paranız şu anda gönderilemiyor, lütfen size yardımcı olması için bir yetkiliyi etiketleyin.",
+
+    OWO_BOT_ID: "408785106942164992",
+
+};
